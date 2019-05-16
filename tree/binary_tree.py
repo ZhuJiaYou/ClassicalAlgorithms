f418b9fb810addb544abefcef49ac104dcaa9ee9
Zhu Jia You <lxfdbzjy@163.com>
2019-05-16 20:53:49 +0800

    Add post traverse binary tree with no recursion

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -19,7 +19,7 @@ def pre_traverse_no_recur(root):
     rights = []
 
     pos = root
-    while((pos != None) or (len(rights) > 0)):
+    while (pos != None) or (len(rights) > 0):
         if pos == None:
             pos = rights.pop()
         else:
@@ -37,6 +37,66 @@ def post_traverse(root):
 
 
 def post_traverse_no_recur(root):
+    if root == None:
+        return
+    fathers = []
+    pos = root
+    last = None
+    fathers.append(root)
+    while (pos != None) or (len(fathers) > 0):
+        if pos == None:
+            pos = fathers.pop()
+        else:
+            if (pos.left == None) and (pos.right == None):
+                print(pos.value)
+                last = pos
+                pos = None
+            elif (pos.left != None) and (pos.right != None):
+                if last == pos.right:
+                    print(pos.value)
+                    last = pos
+                    pos = None
+                elif last == pos.left:
+                    pos = pos.right
+                else:
+                    fathers.append(pos)
+                    pos = pos.left
+            elif pos.left != None:
+                if last == pos.left:
+                    print(pos.value)
+                    last = pos
+                    pos = None
+                else:
+                    fathers.append(pos)
+                    pos = pos.left
+            else:
+                if last == pos.right:
+                    print(pos.value)
+                    last = pos
+                    pos = None
+                else:
+                    fathers.append(pos)
+                    pos = pos.right
+
+
+def post_traverse_no_recur2(root):
+    if root == None:
+        return
+    stack = list()
+    stack.append(root)
+    stack.append(root)
+    pos = root
+    while len(stack) > 0:
+        pos = stack.pop()
+        if (len(stack) > 0) and (pos == stack[-1]):
+            if pos.right != None:
+                stack.append(pos.right)
+                stack.append(pos.right)
+            if pos.left != None:
+                stack.append(pos.left)
+                stack.append(pos.left)
+        else:
+            print(pos.value)
 
 
 def mid_traverse(root):
@@ -52,4 +112,6 @@ if __name__ == '__main__':
     root = BinaryNode('a', BinaryNode('b', BinaryNode('c'), BinaryNode('d')), 
             BinaryNode('e', BinaryNode('f', None, BinaryNode('g'))))
 
-    pre_traverse_no_recur(root)
+    post_traverse_no_recur(root)
+    print("******************************************************")
+    post_traverse_no_recur2(root)
