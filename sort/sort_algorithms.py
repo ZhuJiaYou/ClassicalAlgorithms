923584a6453f7f9aca382e2937175f80078f8124
Zhu Jia You <lxfdbzjy@163.com>
2019-07-18 15:02:11 +0800

    Add selection and insertion and shell ane merge sort

--- sort/sort_algorithms.py
+++ sort/sort_algorithms.py
@@ -1,8 +1,83 @@
+# stable
+# time avg: O(n2), time worst: O(n2), time best: O(n)
+# space: O(1)
 def bubble_sort(arr):
     for i in range(1, len(arr)):  # NOTICE THE <1>
         for j in range(len(arr)-i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+# unstable
+# time avg: O(n2), time worst: O(n2), time best: O(n2)
+# space: O(1)
+def selection_sort(arr):
+    for i in range(len(arr)-1):
+        mini = i
+        for j in range(i+1, len(arr)):
+            if arr[mini] > arr[j]:
+                mini = j
+        arr[i], arr[mini] = arr[mini], arr[i]
+
+
+# stable
+# time avg: O(n2), time worst: O(n2), time best: O(n)
+# space: O(1)
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i-1, -1, -1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+# unstable
+# time avg: O(nlogn2), time worst: O(nlogn2), time best: O(nlogn)
+# space: O(1)
+def shell_sort(arr):
+    k = len(arr) // 2
+    while k > 0:
+        for m in range(k):
+            for n in range(m+k, len(arr), k):
+                for p in range(n-k, -1, -k):
+                    if arr[p] > arr[p+k]:
+                        arr[p], arr[p+k] = arr[p+k], arr[p]
+        k = k // 2
+
+
+# stable
+# time avg: O(nlogn), time worst: O(nlogn), time best: O(nlogn)
+# space: O(n)
+def merge_sort(arr):
+    def merge(left, right):
+        merged = []
+        while left and right:
+            if left[0] <= right[0]:
+                merged.append(left.pop(0))
+            else:
+                merged.append(right.pop(0))
+        return merged + left + right
+
+    if len(arr) < 2:
+        return arr
+    mid = len(arr) // 2
+    return merge(merge_sort(arr[:mid]), merge_sort(arr[mid:]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -12,5 +87,7 @@ def bubble_sort(arr):
 
 if __name__ == '__main__':
     a = [2, 8, 5, 7, 3, 0]
-    bubble_sort(a)
-    print(a)
+    # bubble_sort(a)
+    # selection_sort(a)
+    # insertion_sort(a)
+    print(merge_sort(a))
